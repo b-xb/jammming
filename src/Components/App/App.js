e6f24ebea5a004a68a70505ee9108da6695011fd
b-xb <7443583+b-xb@users.noreply.github.com>
2022-11-29 21:29:20 +0000

    Added Search Results Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
 import './App.css';
 
 class App extends React.Component {
@@ -11,7 +12,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            {/* <SearchResults /> */}
+            <SearchResults />
             {/* <Playlist /> */}
           </div>
         </div>
